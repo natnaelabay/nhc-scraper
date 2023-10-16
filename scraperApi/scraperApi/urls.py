88fea9ea92d30f26f1213528ec1fe5375085422c
Natnael Abay(Nati) <se.natnael.abay@gmail.com>
2023-10-16 15:27:54 +0300

    fix: update route name

--- scraperApi/scraperApi/urls.py
+++ scraperApi/scraperApi/urls.py
@@ -25,7 +25,7 @@ from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Episyche Technologies",
+        title="Bilby Scraper",
         default_version='v1',),
     public=True,
     permission_classes=(permissions.AllowAny,),
