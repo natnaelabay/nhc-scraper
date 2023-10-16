9cd5692b8ce4399b7b12bdb70d57c299ff81e027
Natnael Abay <se.natnael.abay@gmail.com>
2023-10-16 03:28:23 +0300

    fix: add is_scraped field to django admin

--- scraperApi/api/admin.py
+++ scraperApi/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Category, Article
 
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'published_date', 'status', 'url')
+    list_display = ('title', 'published_date', 'status', "is_scraped", 'url', )
     list_filter = ('status',)  # Define the list filter for the 'status' field
 
 
